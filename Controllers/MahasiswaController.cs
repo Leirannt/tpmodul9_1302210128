a3a9ff370ba55673f4dc60403c48f561839ec9d6
Leirannt <93717185+Leirannt@users.noreply.github.com>
2023-04-17 02:51:00 +0700

    menambahkan API mahasiswa

--- Controllers/MahasiswaController.cs
+++ Controllers/MahasiswaController.cs
@@ -11,6 +11,36 @@ namespace tpmodul9_1302210128.Controllers
     [ApiController]
     public class MahasiswaController : ControllerBase
     {
+        private static List<Mahasiswa> mahasiswas = new List<Mahasiswa>()
+        {
+            new  Mahasiswa(){ nama = "Ariel Pabibak Pali' Mallua'", nim = "1302210128"},
+            new  Mahasiswa(){ nama = "Lebron James", nim = "1302000001"},
+            new  Mahasiswa(){ nama = "Stephen Curry", nim = "1302000002"}
+        };
 
+        [HttpGet]
+        public IEnumerable<Mahasiswa> GET()
+        {
+            return mahasiswas;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa murid)
+        {
+            mahasiswas.Add(murid);
+        }
+
+        [HttpGet("{id}")]
+        public void GET(int id, [FromBody] Mahasiswa murid)
+        {
+
+            mahasiswas[id] = murid;
+        }//maaf tapi ini saya belum dapat caranya
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mahasiswas.RemoveAt(id);
+        }
     }
 }
--- Mahasiswa/Mahasiswa.cs
+++ Mahasiswa/Mahasiswa.cs
@@ -5,5 +5,11 @@ using System.Threading.Tasks;
 
 namespace tpmodul9_1302210128
 {
+    public class Mahasiswa
+    {
+        
+        public string nama { get; set; }
+        public string nim { get; set; }
+    }
 
 }
